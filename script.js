db73918c356dbe49f1958df9ce4c30d31d967e6e
keertana7ammanagi-source <keertana7ammanagi@gmail.com>
2025-09-10 14:35:29 +0530

    commit
    commit by me

---
+++ script.js
@@ -0,0 +1,26 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 04, 2022 15:30:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "EXPIRED";
+        document.getElementById("hours").innerHTML = "";
+        document.getElementById("minutes").innerHTML = "";
+        document.getElementById("seconds").innerHTML = "";
+    }
+}, 1000);
